69825b348142d24222ca38853d23e7624a1ac79a
“Hisham <hisham@digitalais.com>
2018-09-17 14:59:56 +0300

    test

--- SportActivities/settings.py
+++ SportActivities/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'SportActivities.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sys',
+        'USER': 'root',
+        'PASSWORD': 'Amman123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
+
 }
 
 
